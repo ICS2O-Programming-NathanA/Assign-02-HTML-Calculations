72ece98201c85dd1c11fd19e8a60884e826b8f96
Nathan Araujo <99274283+nathan-araujo@users.noreply.github.com>
2022-04-05 14:53:24 +0000

    updated js and html

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Copyright (c) 2022 Nathan Araujo All rights reserved
+//
+// Created by: Nathan Araujo
+// Created on: March 2022
+// This file contains the JS functions for index.html
+
+"use strict"
+
+// declare constants
+const UNTRAINED = 0.7456
+const INTERMEDIATE = 1.1403
+const ELITE = 1.9298
+
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function maleButton () {
+  // get user input
+  let weight = parseFloat(document.getElementById('weight_of_person').value)
+
+  // display the results
+  document.getElementById('weight').innerHTML = "You will make $" + weight_of_person.toFixed(2)
+  document.getElementById('weight').innerHTML = "The government will steal $" + weight_of_person.toFixed(2)
+}
