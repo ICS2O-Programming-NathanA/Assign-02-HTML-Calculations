27e8a2a07f4a0aea6f3101a19212d0477ffe9ab0
Nathan Araujo <99274283+nathan-araujo@users.noreply.github.com>
2022-04-06 23:03:41 +0000

    updated js

--- js/script.js
+++ js/script.js
@@ -4,21 +4,48 @@
 // Created on: March 2022
 // This file contains the JS functions for index.html
 
-"use strict"
+'use strict'
+/**
+ * This function calculates how much the person should bench press at the untrained level (male).
+ */
+function calculateUntrainedMale () {
+  // input
+  const weight = parseInt(document.getElementById('weight-of-male-untrained').value)
 
-// declare constants
-const UNTRAINED = 0.7456
-const INTERMEDIATE = 1.1403
-const ELITE = 1.9298
+	// process
+	const untrained = 0.7456
+  const benchpress = (weight * untrained)
+
+  // output
+  document.getElementById('untrainedMale').innerHTML = 'You should bench press around: ' + benchpress + 'lbs'
+}
 
 /**
- * This function calculates area and perimeter of rectangle.
+ * This function calculates how much the person should bench press at the intermediate level (male).
  */
-function maleButton () {
-  // get user input
-  let weight = parseFloat(document.getElementById('weight_of_person').value)
+function calculateIntermediateMale () {
+	// input
+  const weight = parseInt(document.getElementById('weight-of-male-intermediate').value)
 
-  // display the results
-  document.getElementById('weight').innerHTML = "You will make $" + weight_of_person.toFixed(2)
-  document.getElementById('weight').innerHTML = "The government will steal $" + weight_of_person.toFixed(2)
+	// process
+	const untrained = 1.1403
+  const benchpress = (weight * untrained)
+
+  // output
+  document.getElementById('intermediateMale').innerHTML = 'You should bench press around: ' + benchpress + 'lbs'
+}
+
+/**
+ * This function calculates how much the person should bench press at the untrained level (female).
+ */
+function calculateUntrainedFemale () {
+	// input
+  const weight = parseInt(document.getElementById('weight-of-female-untrained').value)
+
+	// process
+	const untrained = 0.5154
+  const benchpress = (weight * untrained)
+
+  // output
+  document.getElementById('untrainedFemale').innerHTML = 'You should bench press around: ' + benchpress + 'lbs'
 }
